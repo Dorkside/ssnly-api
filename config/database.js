a7105a8139f1f66fcc88b64f37047f0d04650cc8
Dorkside <jdmartin@gmx.fr>
2020-06-07 19:45:52 +0200

    fix: env()

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: "${process.env.DATABASE_HOST}",
-        port: "${process.env.DATABASE_PORT}",
-        database: "${process.env.DATABASE_NAME}",
-        username: "${process.env.DATABASE_USERNAME}",
-        password: "${process.env.DATABASE_PASSWORD}",
+        host: end("DATABASE_HOST"),
+        port: end("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
         ssl: { rejectUnauthorized: false },
       },
       options: {
